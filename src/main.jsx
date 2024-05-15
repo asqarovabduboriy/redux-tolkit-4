d7eaf48a24381ae01980c031d3cd395539274d11
Abduboriy <asqarovabduboriy8@gmail.com>
2024-05-15 14:14:53 +0500

    a

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import  {store}  from "./context/store.js";
+import { store } from "./context/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-       <App />
-    </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
